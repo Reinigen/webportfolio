2c0cf61dfa620d1e1de4a79b9ca9fb22868b63a8
Reinigen <raymundcrystal@gmail.com>
2026-01-29 12:45:40 +0800

    Fixed contact form sizing

--- webportfolio/src/app/contact/contact.tsx
+++ webportfolio/src/app/contact/contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact(){
     return(
-        <div id="contact-form" className="me-md-5 me-5 my-5 vh-75">
+        <div id="contact-form" className="me-md-5 p-md-5 me-5 my-5 vh-75">
             <h1 className="text-danger h1">Connect with Me!</h1>
             <p className="pe-3">Does my work align with your needs? If so, feel free to leave a message!</p>
             <div className="pe-5 pt-3 fs-2">
@@ -11,7 +11,7 @@ export default function Contact(){
                 <a href="https://github.com/Reinigen"
                     ><i className="p-2 bi bi-github text-danger"/></a>
             </div>
-            <form action="https://api.web3forms.com/submit" method="POST" className="text-start pe-3 mb-5">
+            <form action="https://api.web3forms.com/submit" method="POST" className="text-start pe-5 me-5 mb-5">
                 <input type="hidden" name="access_key" value="72ae4d1c-4d25-4178-9d35-0f91acc5e8e0" />
                 <div className="mb-3">
                 <label htmlFor="name" className="form-label">Full Name: </label>
